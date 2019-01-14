56519017fb3ece3281cac516a7cf9965f1dbf233
Guilherme Luis <guile.hm@hotmail.com>
2019-01-14 10:35:07 -0200

    Change allowed hosts and static root for deploy

--- aws/aws/settings.py
+++ aws/aws/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1t!*&r6o-rflujfo6x%o1*s*9cf44^pcq7k5$bmehuyi)_=h_1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,3 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
